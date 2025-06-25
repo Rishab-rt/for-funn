3c31af76faccb6170c9015994b1dcd7a6d55c0c6
Rishab-rt <rtalusani07@gmail.com>
2025-06-25 15:25:05 -0400

    Update financeCalculator.py

--- financeCalculator.py
+++ financeCalculator.py
@@ -7,7 +7,7 @@ balance = 0
 def trackerRun():
     global balance, transactions
     while True:
-        type = input("Enter transaction type (Income/Expense) or 'exit': ")
+        type = input("Enter transaction type (Income/Expense) or 'exit' or 'view balance' or 'view transaction history': ")
         if type == "exit":
            exit()
         elif type == "Expense":
@@ -16,38 +16,46 @@ def trackerRun():
         elif type == "Income":
            addIncome()
            continue
+        elif type == "view balance":
+            viewBalance()
+            continue
+        elif type == "view transaction history":
+            viewTransactionHistory()
+            continue
         else:
            trackerRun()
     
-    check = input("Would you like to view previous transactions?: ")
-    if check == "yes":
-      viewTransactionHistory()
+    
     
       
 def addIncome():
-  amount = int(input("Please enter the amount that you have earned: $"))
-  balance += amount
-  type = input("What is the category?: ")
-  incomeList.append(type)
+    global balance, transactions
+    amount = int(input("Please enter the amount that you have earned: $"))
+    balance += amount
+    type = input("What is the category?: ")
+    incomelist.append(type)
 
 def addTransaction():
-  amount = int(input("Please enter the amount that you have spent: $"))
-  balance -= amount 
-  expenseamt.append(amount)
-  type = input("What is the category?: ")
-  expenseList.append(type)
-  transactions+=1
+    global balance, transactions
+    amount = int(input("Please enter the amount that you have spent: $"))
+    balance -= amount 
+    expenseamt.append(amount)
+    type = input("What is the category?: ")
+    expenselist.append(type)
+    transactions+=1
 
 def viewBalance():
-  view = input("Would you like to view balance?: ")
-  if view == "yes":
-    print("You have a balance of $", balance)
+    global balance, transactions
+    view = input("Would you like to view balance?: ")
+    if view == "yes":
+        print("You have a balance of $", balance)
     
 
 
 def viewTransactionHistory():
-  for i in range (transactions):
-    print(expenseList[i],"$",expenseamt[i])
+    global balance, transactions
+    for i in range (transactions):
+        print(expenselist[i],"$",expenseamt[i])
   
 
 trackerRun()
